3cea91156276587b4ef00e7f22d519ed8f584892
TopDev <nguyen91.trai@outlook.com>
2024-07-31 22:20:14 +1100

    create home page

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,274 @@
+import React, {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
+import {ActivityIndicator, Colors} from 'react-native-paper';
+import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {useIsFocused} from '@react-navigation/native';
+import {getUser} from '../../actions/profileAction';
+import {getPrice} from '../../actions/exchangeAction';
+import {getHistoryById} from '../../actions/historyAction';
+import animal from '../../assets/img/doge.png';
+import ethImg from '../../assets/img/eth.png';
+import usdtImg from '../../assets/img/usdt.png';
+import isEmpty from '../../utils/isEmpty';
+import moment from 'moment';
+
+const Home = ({navigation, props}) => {
+  const dispatch = useDispatch();
+  const isFocused = useIsFocused();
+  const [money, setMoney] = useState(0);
+  const price = useSelector(state => state.price);
+  const store = useSelector(state => state.auth);
+  const history = useSelector(state => state.history);
+  const socket = useSelector(state => state.socket);
+  const profile = useSelector(state => state.profile);
+  useEffect(() => {
+    let isMount = true;
+    if (isMount) {
+      dispatch(getPrice());
+      dispatch(getUser(store.user.id));
+      dispatch(getHistoryById(store.user.address));
+    }
+    return () => {
+      isMount = false;
+    };
+  }, [isFocused, props]);
+  useEffect(() => {
+    let isMount = true;
+    if (isMount) {
+      if (
+        !isEmpty(socket.socket) &&
+        !isEmpty(price.pricedata) &&
+        !isEmpty(profile.profiledata)
+      )
+        socket.socket.on('price', item => {
+          setMoney(
+            price.pricedata.price * profile.profiledata.countHDT +
+              Number(item.price) * profile.profiledata.countETH +
+              profile.profiledata.countUSDT,
+          );
+        });
+    }
+    return () => {
+      isMount = false;
+    };
+  }, [socket, price, profile, isFocused, props]);
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.menuDiv}>
+          <Icon
+            style={{paddingLeft: 10}}
+            name="bars"
+            size={30}
+            color={'rgb(223,100,71)'}
+            onPress={() => {
+              navigation.toggleDrawer();
+            }}
+          />
+        </View>
+        <ScrollView>
+          <View style={styles.scrollMenu}>
+            <View style={styles.imgDiv}>
+              <Image style={styles.img} source={animal} />
+            </View>
+            <View style={styles.curDiv}>
+              <Text style={styles.curMoney}>${money.toFixed(2)}</Text>
+            </View>
+            <View style={styles.rowDiv}>
+              <Image style={styles.imgUnit} source={animal} />
+              <Text style={styles.tokenCount}>
+                {isEmpty(profile.profiledata)
+                  ? ''
+                  : profile.profiledata.countHDT.toFixed(2)}
+              </Text>
+            </View>
+            <View style={styles.rowDiv}>
+              <Image style={styles.imgUnit} source={ethImg} />
+              <Text style={styles.curMoney}>
+                {isEmpty(profile.profiledata)
+                  ? ''
+                  : profile.profiledata.countETH.toFixed(2)}
+              </Text>
+            </View>
+            <View style={styles.rowDiv}>
+              <Image style={styles.imgUnit} source={usdtImg} />
+              <Text style={styles.curMoney}>
+                {isEmpty(profile.profiledata)
+                  ? ''
+                  : profile.profiledata.countUSDT.toFixed(2)}
+              </Text>
+            </View>
+            <View>
+              <View style={styles.btnGroup}>
+                <TouchableOpacity
+                  style={styles.submitButtonStyle}
+                  activeOpacity={0.5}
+                  onPress={() => navigation.navigate('Stacking')}>
+                  <Text style={styles.TextStyle}>STACKING</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.submitButtonStyle}
+                  activeOpacity={0.5}
+                  onPress={() => navigation.navigate('Transfer')}>
+                  <Text style={styles.TextStyle}>TRANSFER</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.btnGroup}>
+                <TouchableOpacity
+                  style={styles.submitButtonStyle}
+                  activeOpacity={0.5}
+                  onPress={() => navigation.navigate('SwapToEth')}>
+                  <Text style={styles.TextStyle}>SWAP TO ETH</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.submitButtonStyle}
+                  activeOpacity={0.5}
+                  onPress={() => navigation.navigate('SwapToHDT')}>
+                  <Text style={styles.TextStyle}>SWAP TO HDT</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View>
+              <Text style={styles.transText}>LATEST TRANSACTIONS</Text>
+            </View>
+
+            <View>
+              {history.loading ? (
+                <ActivityIndicator
+                  animating={true}
+                  size="large"
+                  color={Colors.red800}
+                />
+              ) : (
+                history.historydata.map((item, key) => {
+                  if (key < 4) {
+                    return (
+                      <View style={styles.historyDiv} key={key + 1}>
+                        <View style={styles.rowDiv}>
+                          <View style={styles.circle} />
+                          <View>
+                            {item.type === 1 ? (
+                              item.method === 'eth' ? (
+                                <Text>
+                                  Deposit Completed.{' '}
+                                  {Number(item.amount).toFixed(2)} ETH
+                                </Text>
+                              ) : item.method === 'hdt' ? (
+                                <Text>
+                                  Deposit Completed.{' '}
+                                  {Number(item.amount).toFixed(2)} HDT
+                                </Text>
+                              ) : (
+                                ''
+                              )
+                            ) : item.type === 2 ? (
+                              item.method === 'eth' ? (
+                                <Text>
+                                  Withdraw Completed.{' '}
+                                  {Number(item.amount).toFixed(2)} ETH
+                                </Text>
+                              ) : item.method === 'hdt' ? (
+                                <Text>
+                                  Withdraw Completed.{' '}
+                                  {Number(item.amount).toFixed(2)} HDT
+                                </Text>
+                              ) : item.method === 'usdt' ? (
+                                <Text>
+                                  Withdraw Completed.{' '}
+                                  {Number(item.amount).toFixed(2)} USDT
+                                </Text>
+                              ) : (
+                                ''
+                              )
+                            ) : item.type === 3 ? (
+                              item.method === 'eth' ? (
+                                <Text>
+                                  Transferred to{' '}
+                                  {item.to_address
+                                    ? item.to_address.substring(0, 3) +
+                                      '...' +
+                                      item.to_address.substring(
+                                        item.to_address.length - 3,
+                                        item.to_address,
+                                      )
+                                    : ''}{' '}
+                                  {Number(item.amount).toFixed(2)} ETH
+                                </Text>
+                              ) : item.method === 'hdt' ? (
+                                <Text>
+                                  Transferred to{' '}
+                                  {item.to_address
+                                    ? item.to_address.substring(0, 3) +
+                                      '...' +
+                                      item.to_address.substring(
+                                        item.to_address.length - 3,
+                                        item.to_address,
+                                      )
+                                    : ''}{' '}
+                                  {Number(item.amount).toFixed(2)} HDT
+                                </Text>
+                              ) : (
+                                ''
+                              )
+                            ) : item.type === 4 ? (
+                              <Text>
+                                HDT Staked Successfully{' '}
+                                {Number(item.amount).toFixed(2)} HDT
+                              </Text>
+                            ) : item.type === 5 ? (
+                              item.method === 'eth' ? (
+                                <Text>
+                                  {Number(item.amount).toFixed(2)} ETH Swapped
+                                  to HDT
+                                </Text>
+                              ) : item.method === 'hdt' ? (
+                                <Text>
+                                  {Number(item.amount).toFixed(2)} HDT Swapped
+                                  to ETH
+                                </Text>
+                              ) : (
+                                ''
+                              )
+                            ) : item.type === 6 ? (
+                              <Text>
+                                Referral Commission.{' '}
+                                {Number(item.amount).toFixed(2)} USDT
+                              </Text>
+                            ) : item.type === 7 ? (
+                              <Text>
+                                Staking Reward Added to Balance{' '}
+                                {Number(item.amount).toFixed(2)} HDT
+                              </Text>
+                            ) : (
+                              ''
+                            )}
+                            <Text>
+                              {moment(item.date).format('DD/MM/YYYY hh:mm A')}
+                            </Text>
+                          </View>
+                        </View>
+                      </View>
+                    );
+                  }
+                })
+              )}
+            </View>
+
+            <View style={styles.footTxt}>
+              <Text
+                style={styles.moreText}
+                onPress={() =>
+                  navigation.navigate('MoreHistory')
+                }>{`more>>`}</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Home/styles.js
@@ -0,0 +1,123 @@
+import {StyleSheet, Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    minHeight: height,
+  },
+  scrollMenu: {
+    alignItems: 'center',
+  },
+  imgDiv: {
+    borderRadius: 100,
+    padding: 15,
+    borderWidth: 15,
+    borderColor: 'rgb(102,59,8)',
+    textAlign: 'center',
+  },
+  img: {
+    width: 50,
+    height: 50,
+    borderWidth: 1,
+  },
+  curDiv: {
+    marginTop: 20,
+  },
+  curMoney: {
+    marginLeft: 5,
+    fontSize: 23,
+    color: 'rgb(119,118,120)',
+  },
+  tokenCount: {
+    fontSize: 23,
+    marginLeft: 5,
+    color: 'rgb(223,100,71)',
+  },
+  submitButtonStyle: {
+    marginTop: 15,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    backgroundColor: 'rgb(223,100,71)',
+    borderRadius: 10,
+    borderWidth: 1,
+    width: 130,
+    borderColor: '#fff',
+  },
+  TextStyle: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  rowDiv: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  historyDiv: {
+    width: width,
+    padding: 10,
+    paddingRight: 40,
+    paddingLeft: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  menuDiv: {
+    width: width,
+    marginTop: 10,
+    paddingRight: 10,
+    paddingLeft: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  btnGroup: {
+    width: width,
+    paddingRight: 50,
+    paddingLeft: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  transText: {
+    marginTop: 20,
+    color: 'rgb(1,154,213)',
+  },
+  circle: {
+    backgroundColor: 'rgb(223,100,71)',
+    height: 40,
+    width: 40,
+    marginRight: 10,
+    borderRadius: 100,
+  },
+  moreText: {
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: 'right',
+    color: 'rgb(1,154,213)',
+    marginRight: 20,
+  },
+  imgUnit: {
+    width: 20,
+    height: 20,
+  },
+  footTxt: {
+    width: width,
+    justifyContent: 'flex-end',
+    marginBottom: 30,
+  },
+  iconDiv: {
+    flexDirection: 'row',
+  },
+  headText: {
+    color: 'rgb(102,59,8)',
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 30,
+  },
+});
+export default styles;
