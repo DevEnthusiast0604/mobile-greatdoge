7fe95a62b0eb1f231efe9bb061d8a7239d4bccb0
TopDev <nguyen91.trai@outlook.com>
2024-08-29 14:27:11 +1100

    constant

---
+++ frontend/src/constant/history.js
@@ -0,0 +1,49 @@
+export const subText = [
+  {
+    description: 'Received payment',
+  },
+
+  {
+    description: 'Transfer money',
+  },
+  {
+    description: 'Swapped to HDT',
+  },
+  {
+    description: 'Swapped to ETH',
+  },
+  {
+    description: 'Withdraw is pending',
+  },
+  {
+    description: 'Withdraw is rejected',
+  },
+  {
+    description: 'Withdraw is success',
+  },
+];
+export const mainText = [
+  {text: 'Your referral'},
+  {text: 'ETH Deposit'},
+  {text: 'ETH Withdraw'},
+  {text: 'ETH Swapped'},
+  {text: 'HDT Swapped'},
+  {text: 'HDT Deposit'},
+  {text: 'HDT Withdraw'},
+  {text: 'Staked Money'},
+  {text: 'Swapped Money'},
+];
+
+export const newText = [
+  {text: 'Deposit Transaction'},
+  {text: 'Referral Transaction'},
+  {text: 'Swap To HDT'},
+  {text: 'Swap To ETH'},
+  {text: 'ETH Transfer'},
+  {text: 'HDT Transfer'},
+  {text: 'Staking Transaction'},
+  {text: 'Completed Staking'},
+  {text: 'ETH withdrawal'},
+  {text: 'HDT withdrawal'},
+  {text: 'USDT withdrawal'},
+];
