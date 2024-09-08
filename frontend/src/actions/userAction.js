9d476848ec84f1bb631c54e4ad28e0c6fb94b5f9
TopDev <nguyen91.trai@outlook.com>
2024-09-09 04:21:10 +1100

    user action

---
+++ frontend/src/actions/userAction.js
@@ -0,0 +1,106 @@
+import axios from 'axios';
+import {SERVER_URL} from '../constant/server_url';
+import {
+  GET_USER_DATA,
+  GET_SELECTED_USER,
+  USER_LOADING,
+  GET_ERRORS,
+  GET_USER_REFERRAL_DATA,
+  GET_USER_STAKE_DATA,
+  GET_USER_TRANSFER_DATA,
+  GET_USER_WITHDRAW_DATA,
+} from './type';
+
+export const getAllUser = () => {
+  return async dispatch => {
+    dispatch({type: USER_LOADING});
+    await axios
+      .get(SERVER_URL + '/users/all')
+      .then(res => {
+        dispatch({type: GET_USER_DATA, payload: res.data});
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
+
+export const getUserById = id => {
+  return async dispatch => {
+    dispatch({type: USER_LOADING});
+    await axios
+      .post(SERVER_URL + '/users/getuser', {id})
+      .then(res => {
+        dispatch({type: GET_SELECTED_USER, payload: res.data});
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
+
+export const getStakeData = ID => {
+  return async dispatch => {
+    await axios
+      .post(SERVER_URL + '/stack/getStake', {ID})
+      .then(res => {
+        dispatch({type: GET_USER_STAKE_DATA, payload: res.data});
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
+
+export const getTransferData = address => {
+  return async dispatch => {
+    await axios
+      .post(SERVER_URL + '/history/gethistory', {address})
+      .then(res => {
+        dispatch({type: GET_USER_TRANSFER_DATA, payload: res.data});
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
+
+export const getWithdrawData = address => {
+  return async dispatch => {
+    await axios
+      .post(SERVER_URL + '/withdraw/getwithdraw', {address})
+      .then(res => {
+        dispatch({type: GET_USER_WITHDRAW_DATA, payload: res.data});
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
+
+export const getReferralData = address => {
+  return async dispatch => {
+    await axios
+      .post(SERVER_URL + '/referral/getreferral', {address})
+      .then(res => {
+        dispatch({type: GET_USER_REFERRAL_DATA, payload: res.data});
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
+
+export const setBalance = data => {
+  return async dispatch => {
+    await axios
+      .post(SERVER_URL + '/users/setbalance', data)
+      .then(res => {
+        dispatch(getUserById(data.ID));
+        dispatch(getAllUser());
+      })
+      .catch(err => {
+        dispatch({type: GET_ERRORS, payload: err.response.data});
+      });
+  };
+};
